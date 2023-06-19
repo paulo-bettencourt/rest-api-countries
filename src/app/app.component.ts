f0f41942f188219fc76da9460afc682a5c5c0cf6
Paulo Bettencourt <pauloblemos@gmail.com>
2023-06-19 16:49:46 +0000

    css refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,20 @@ import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { SearchComponent } from './pages/search/search.component';
 
 @Component({
   standalone: true,
   selector: 'rest-api-countries-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [NxWelcomeComponent, RouterModule, FooterComponent, HeaderComponent],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    FooterComponent,
+    HeaderComponent,
+    SearchComponent,
+  ],
 })
 export class AppComponent {
   title = 'rest-api-countries';
